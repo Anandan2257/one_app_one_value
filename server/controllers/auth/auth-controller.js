5b64cd37219353cc9febee9ca4e50bb5730318a7
Anandan <151502643+Anandan2257@users.noreply.github.com>
2025-01-17 16:20:37 +0530

    Update auth-controller.js

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -68,7 +68,7 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: true }).json({
+   /* res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in successfully",
       user: {
@@ -77,7 +77,18 @@ const loginUser = async (req, res) => {
         id: checkUser._id,
         userName: checkUser.userName,
       },
-    });
+    });*/
+
+    res.status(200).json({
+      success : true,
+      message : 'Logges in successfully',
+      token,user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+        userName: checkUser.userName,
+      },
+    })
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -98,7 +109,8 @@ const logoutUser = (req, res) => {
 
 //auth middleware
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.token;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
   if (!token)
     return res.status(401).json({
       success: false,
